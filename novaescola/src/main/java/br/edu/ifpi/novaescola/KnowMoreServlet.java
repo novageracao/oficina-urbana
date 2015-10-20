bf2a74666b5bf443eb1d5dd97272db655ace7070
Ismael Sarmento <ismael.sarmento.jr@gmail.com>
2015-10-19 22:05:05 -0300

    fixed responsive graph for NE system

---
+++ novaescola/src/main/java/br/edu/ifpi/novaescola/KnowMoreServlet.java
@@ -0,0 +1,21 @@
+package br.edu.ifpi.novaescola;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class KnowMoreServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 3465766415338782800L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("html/know-more.html");
+		dispatcher.forward(req, resp);
+	}
+
+}
